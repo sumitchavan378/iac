ffa0db1b756927581e21efa61a106d632644dbfe
sumitchavan378 <45897342+sumitchavan378@users.noreply.github.com>
2023-01-17 13:04:41 +0530

    Update provider.tf

--- aws_iam/provider.tf
+++ aws_iam/provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
-    access_key = "AKIA44YNO3GUB2BHXYCH"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "us-east-1"
 }
